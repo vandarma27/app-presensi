a23578acbcf2a7cfaa9705fab7375abc608ea6e0
Firman Diatullah Guna Darma <darmaroyale@gmail.com>
2022-11-05 15:56:23 +0700

    Update users.js
    menambahkan encryp

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const { where } = require('sequelize')
+// const { where } = require('sequelize')
 const router = express.Router()
 const UsersModel = require('../models/users')
+const bcrypt = require('bcrypt');
 
 // routing endpoint users utama
 router.get('/', async (req, res) => {
@@ -15,8 +16,11 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
     const { nip, nama, password } = req.body
 
+    const encryptedPassword = await bcrypt.hash(password, 10)
+
+
     const users = await UsersModel.create({
-        nip, nama, password
+        nip, nama, password: encryptedPassword
     })
     res.status(200).json({
         data: users,
@@ -26,14 +30,17 @@ router.post('/', async (req, res) => {
 
 router.put('/', async (req, res) => {
     const { nip, nama, password, passwordBaru } = req.body
-
     const userData = await UsersModel.findOne({ where: { nip: nip } })
 
+    const compare = await bcrypt.compare(password, userData.password)
+    const encryptedPassword = await bcrypt.hash(passwordBaru, 10)
+
     //password yang muncul di db === password dari inputan
-    if (userData.password === password) {
+    if (compare === true) {
         const users = await UsersModel.update({
-            nama, password: passwordBaru
+            nama, password: encryptedPassword
         }, { where: { nip: nip } })
+        
 
         res.status(200).json({
             users : {update : users[0]},
@@ -44,11 +51,6 @@ router.put('/', async (req, res) => {
             error : "data invalid"
         })
     }
-
-    // res.status(200).json({
-    //     data: users,
-    //     metadata: "test post user endpoints"
-    // })
 })
 
 
