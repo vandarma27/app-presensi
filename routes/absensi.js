e49c8318f5d379a2f7d7701631250afa7131a444
Firman Diatullah Guna Darma <darmaroyale@gmail.com>
2022-11-05 16:24:27 +0700

    nambah route absen

--- routes/absensi.js
+++ routes/absensi.js
@@ -3,12 +3,41 @@ const express = require('express')
 const router = express.Router()
 const AbsensiModel = require('../models/absensi')
 const bcrypt = require('bcrypt');
+const { route } = require('./users');
 
 // routing endpoint users utama
 router.get('/', async (req, res) => {
     const absensi = await AbsensiModel.findAll()
     res.status(200).json({
         data: absensi,
-        metadata: "test user endpoints"
+        metadata: "test absensi endpoints"
     })
 })
+
+router.post('/checkin', async (req, res) => {
+    const {nip} = req.body
+
+    const absensi = await AbsensiModel.create({
+        users_nip : nip, status : 'in'
+    })
+
+    res.status(200).json({
+        data: absensi,
+        metadata: "test absensi in endpoints"
+    })
+})
+
+router.post('/checkout', async (req, res) => {
+    const {nip} = req.body
+
+    const absensi = await AbsensiModel.create({
+        users_nip : nip, status : 'out'
+    })
+    
+    res.status(200).json({
+        data: absensi,
+        metadata: "test absensi out endpoints"
+    })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const sequelize = require('./db.config')
 sequelize.sync().then(() => console.log('database ready!'))
 
 const userEndpoint = require('./routes/users')
+const absensiEndpoint = require('./routes/absensi')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 app.use('/users', userEndpoint)
+app.use('/absensi', absensiEndpoint)
 
 
 app.listen(port, () => console.log(`running server on port ${port}`))
