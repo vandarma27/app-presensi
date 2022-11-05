d0ee662c8f0ab5bbc9b18e6a9044c536756604a7
Firman Diatullah Guna Darma <darmaroyale@gmail.com>
2022-11-05 15:36:21 +0700

    update fitur

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { where } = require('sequelize')
 const router = express.Router()
 const UsersModel = require('../models/users')
 
@@ -22,5 +23,34 @@ router.post('/', async (req, res) => {
         metadata: "test post user endpoints"
     })
 })
+
+router.put('/', async (req, res) => {
+    const { nip, nama, password, passwordBaru } = req.body
+
+    const userData = await UsersModel.findOne({ where: { nip: nip } })
+
+    //password yang muncul di db === password dari inputan
+    if (userData.password === password) {
+        const users = await UsersModel.update({
+            nama, password: passwordBaru
+        }, { where: { nip: nip } })
+
+        res.status(200).json({
+            users : {update : users[0]},
+            metadata: "add updated"
+        })
+    } else {
+        res.status(400).json({
+            error : "data invalid"
+        })
+    }
+
+    // res.status(200).json({
+    //     data: users,
+    //     metadata: "test post user endpoints"
+    // })
+})
+
+
 
 module.exports = router
